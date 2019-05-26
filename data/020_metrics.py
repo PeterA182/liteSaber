9ba887aaf2f56100bcfdfd4c3fc9bad993a91a87
PeterA182 <Altamura.Peter@gmail.com>
2019-05-25 22:47:17 -0400

    initial commit -- metrics creation script following standardization

---
+++ data/020_metrics.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import datetime as dt
+import pandas as pd
+import numpy as np
+CONFIG = util.load_config()
+
+
+def process_date_games(path):
+    """
+    """
+
+    #
+
+
+    return 0
+
+
+if __name__ == "__main__":
+
+    # Run Log
+    min_date = dt.datetime(year=2018, month=8, day=1)
+    max_date = dt.datetime(year=2018, month=8, day=2)
+
+    # Iterate over years
+    years = [y for y in np.arange(min_date.year, max_date.year+1, 1)]
+    dates = [min_date+dt.timedelta(days=i)
+             for i in range((max_date-min_date).days+1)]
+
+    # Establish path
+    path = CONFIG.get('paths').get('normalized')
+    for dd in dates:
+        dd = dd.strftime('year_%Ymonth_%mday_%d/')
+        path += dd         
+        print(path)
+        process_date_games(path)
