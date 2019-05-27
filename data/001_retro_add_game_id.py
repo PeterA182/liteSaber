9865d6974239cd2bd7090e7e342d72453d8ddbf3
PeterA182 <Altamura.Peter@gmail.com>
2019-05-26 20:06:34 -0400

    add game id retro fixed for remaining game - now deprecated

--- data/001_retro_add_game_id.py
+++ data/001_retro_add_game_id.py
@@ -13,7 +13,7 @@ def add_game_id_to_parquet(path):
     # Iterate over files in each dir
     for fname in [x for x in os.listdir(path) if ".parquet" in x]:
         df = pd.read_parquet(path + "/" + fname)
-        df['game_id'] = str(path.split("/")[len(path.split("/"))-1])
+        #df['game_id'] = str(path.split("/")[len(path.split("/"))-1])
         df.to_parquet(path + "/" + fname)
         print("'game_id' added to file: {}".format(str(fname)))
 
@@ -25,9 +25,9 @@ def add_game_id_to_csv(path):
 
     # Iterate over files in each dir
     for fname in [x for x in os.listdir(path) if ".csv" in x]:
-        df = pd.read_csv(path+"/"+fname)
-        df['game_id'] = str(path.split("/")[len(path.split("/"))-1])
-        df.to_csv(path+"/"+fname)
+        df = pd.read_csv(path+"/"+fname, dtype=str)
+        #df['game_id'] = str(path.split("/")[len(path.split("/"))-1])
+        #df.to_csv(path+"/"+fname)
         df.to_parquet(path+"/"+fname.replace(".csv", ".parquet"))
 
 
@@ -38,11 +38,11 @@ if __name__ == "__main__":
 
     # Get list of dirs containing files
     dirs = os.listdir(path)
-    dirs = [x for x in dirs if all(y in x for y in ['year_', 'month_', 'day_'])]
+    dirs = [x for x in dirs if all(y in x for y in ['year_2019', 'month_', 'day_'])]
     paths = [path + dir_ for dir_ in dirs]
 
     # Iterate over files in each dir
     for path in paths:
-        add_game_id_to_parquet(path=path)
+        #add_game_id_to_parquet(path=path)
         add_game_id_to_csv(path=path)
         
