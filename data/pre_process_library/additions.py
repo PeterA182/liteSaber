5db39c2511586961ea6a9dc176ff52023c2b1503
PeterA182 <Altamura.Peter@gmail.com>
2019-05-26 20:08:11 -0400

    preprocessing steps for adding fields

---
+++ data/pre_process_library/additions.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import utilities as util
+CONFIG = util.load_config()
+
+
+def add_teams(data, path):
+    """
+    Parse team information from path and add to table
+    
+    """
+
+    
