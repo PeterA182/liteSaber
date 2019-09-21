6aa09edd2ee83dafdc71db8d9cd896df76c4f708
PeterA182 <Altamura.Peter@gmail.com>
2019-09-20 22:03:29 -0400

    updated daily script and still working on gridsearch

--- data/042_hr_prop_model.py
+++ data/042_hr_prop_model.py
@@ -4,9 +4,24 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score
+
 from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split, KFold, StratifiedKFold
+
+from sklearn.metrics import roc_curve, precision_recall_curve, auc, make_scorer, recall_score, accuracy_score, precision_score, confusion_matrix, f1_score
+
+
+# Run options
+brute = False
+gridsearch = True
+
+# Param Grid
+param_grid = {
+    'min_samples_split': [3, 5, 10], 
+    'n_estimators' : [100, 300],
+    'max_depth': [3, 5, 15, 25],
+    'max_features': [3, 5, 10, 20]
+}
+
 
 # Read in
 path = '/Volumes/Samsung_T5/mlb/gdApi/99_hr_prop_ft/featurespace_all.parquet'
@@ -37,117 +52,103 @@ print(X_test.shape)
 print(y_train.shape)
 print(y_test.shape)
 
-rf_clf = RandomForestClassifier(criterion='gini',
-                                n_estimators=45,
-                                min_samples_split=4,
-                                min_samples_leaf=2,
-                                max_features=0.33,
-                                oob_score=True,
-                                random_state=1,
-                                n_jobs=-1)
-rf_clf.fit(X_train, y_train)
-yhat = rf_clf.predict(X_test)
-pp = rf_clf.predict_proba(X_test)[:, 1]
-f1_score_rf = f1_score(y_test, yhat)
-acc_rf = accuracy_score(y_test, yhat)
+if brute:
+    rf_clf = RandomForestClassifier(criterion='gini',
+                                    n_estimators=45,
+                                    min_samples_split=4,
+                                    min_samples_leaf=2,
+                                    max_features=0.33,
+                                    oob_score=True,
+                                    random_state=1,
+                                    n_jobs=-1)
+    rf_clf.fit(X_train, y_train)
+    yhat = rf_clf.predict(X_test)
+    pp = rf_clf.predict_proba(X_test)[:, 1]
+    f1_score_rf = f1_score(y_test, yhat)
+    acc_rf = accuracy_score(y_test, yhat)
 
-print("F1 Score")
-print(f1_score_rf)
-print("Acc Score")
-print(acc_rf)
+    print("F1 Score")
+    print(f1_score_rf)
+    print("Acc Score")
+    print(acc_rf)
 
-df_comp = pd.DataFrame({
-    'yhat': list(yhat),
-    'y_test': list(y_test),
-    'prob': list(pp)
-})
-df_comp.to_csv('/Users/peteraltamura/Desktop/rf_clf_hit_prop.csv', index=False)
-df_comp['0_pred_correct'] = (
-    (df_comp['yhat'] == 0)
-    &
-    (df_comp['y_test'] == 0)
-)
-df_comp['1_pred_correct'] = (
-    (df_comp['yhat'] == 1)
-    &
-    (df_comp['y_test'] == 1)
-)
-df_comp['0_pred_incorrect'] = (
-    (df_comp['yhat'] == 0)
-    &
-    (df_comp['y_test'] == 1)
-)
-df_comp['1_pred_incorrect'] = (
-    (df_comp['yhat'] == 1)
-    &
-    (df_comp['y_test'] == 0)
-)
-print("Avg 0_pred_correct")
-print(np.mean(df_comp['0_pred_correct']))
-print("Avg 1_pred_correct")
-print(np.mean(df_comp['1_pred_correct']))
-print("Avg 0_pred_incorrect")
-print(np.mean(df_comp['0_pred_incorrect']))
-print("Avg 1_pred_incorrect")
-print(np.mean(df_comp['1_pred_incorrect']))
-print("---- ---- ---- ----\n"*4)
+    df_comp = pd.DataFrame({
+        'yhat': list(yhat),
+        'y_test': list(y_test),
+        'prob': list(pp)
+    })
+    df_comp.to_csv('/Users/peteraltamura/Desktop/rf_clf_hit_prop.csv', index=False)
+    df_comp['0_pred_correct'] = (
+        (df_comp['yhat'] == 0)
+        &
+        (df_comp['y_test'] == 0)
+    )
+    df_comp['1_pred_correct'] = (
+        (df_comp['yhat'] == 1)
+        &
+        (df_comp['y_test'] == 1)
+    )
+    df_comp['0_pred_incorrect'] = (
+        (df_comp['yhat'] == 0)
+        &
+        (df_comp['y_test'] == 1)
+    )
+    df_comp['1_pred_incorrect'] = (
+        (df_comp['yhat'] == 1)
+        &
+        (df_comp['y_test'] == 0)
+    )
+    print("Avg 0_pred_correct")
+    print(np.mean(df_comp['0_pred_correct']))
+    print("Avg 1_pred_correct")
+    print(np.mean(df_comp['1_pred_correct']))
+    print("Avg 0_pred_incorrect")
+    print(np.mean(df_comp['0_pred_incorrect']))
+    print("Avg 1_pred_incorrect")
+    print(np.mean(df_comp['1_pred_incorrect']))
+    print("---- ---- ---- ----\n"*4)
 
-# GridSearch
-param_grid = [
-        {'n_estimators': [7, 8, 10], 
-         'max_features': [0.1, 0.3, 0.4], 
-         'min_samples_split': [10], 
-         'min_samples_leaf': [10, 12, 15],
-         'n_jobs': [-1]}]
-rf_reg = RandomForestClassifier()
-grid_search = GridSearchCV(rf_reg, param_grid, cv=5, scoring="neg_mean_squared_error")
-grid_search.fit(X_train, y_train)
-print(param_grid)
-print(grid_search.best_params_)
+    # GridSearch
+    param_grid = [
+            {'n_estimators': [7, 8, 10], 
+             'max_features': [0.1, 0.3, 0.4], 
+             'min_samples_split': [10], 
+             'min_samples_leaf': [10, 12, 15],
+             'n_jobs': [-1]}]
+    rf_reg = RandomForestClassifier()
+    grid_search = GridSearchCV(rf_reg, param_grid, cv=5, scoring="neg_mean_squared_error")
+    grid_search.fit(X_train, y_train)
+    print(param_grid)
+    print(grid_search.best_params_)
 
-rf_clf_optimal = RandomForestClassifier(
-    n_estimators=grid_search.best_params_.get('n_estimators'), 
-    criterion='gini',
-    max_depth=None,
-    min_samples_split=grid_search.best_params_.get('min_samples_split'),
-    min_samples_leaf=grid_search.best_params_.get('min_samples_leaf'),
-    max_features=grid_search.best_params_.get('max_features')
-)
-rf_clf_optimal.fit(X_train, y_train)
-yhat = rf_clf_optimal.predict(X_test)
-pp = rf_clf_optimal.predict_proba(X_test)[:, 1]
-f1_score_rf = f1_score(y_test, yhat)
-acc_rf = accuracy_score(y_test, yhat)
+elif gridsearch:
+    
+    def grid_search_wrapper(refit_score='precision_score'):
+        """
+        fits a GridSearchCV classifier using refit_score for optimization
+        prints classifier performance metrics
+        """
+        skf = StratifiedKFold(n_splits=10)
+        grid_search = GridSearchCV(clf, param_grid, scoring=scorers, refit=refit_score,
+                               cv=skf, return_train_score=True, n_jobs=-1)
+        grid_search.fit(X_train.values, y_train.values)
 
-print("F1 Score")
-print(f1_score_rf)
-print("Acc Score")
-print(acc_rf)
+        # make the predictions
+        y_pred = grid_search.predict(X_test.values)
 
-df_comp = pd.DataFrame({
-    'yhat': list(yhat),
-    'y_test': list(y_test),
-    'prob': list(pp)
-})
-df_comp.to_csv('/Users/peteraltamura/Desktop/rf_clf_hit_prop_optimal.csv', index=False)
-df_comp['0_pred_correct'] = (
-    (df_comp['yhat'] == 0)
-    &
-    (df_comp['y_test'] == 0)
-)
-df_comp['1_pred_correct'] = (
-    (df_comp['yhat'] == 1)
-    &
-    (df_comp['y_test'] == 1)
-)
-df_comp['0_pred_incorrect'] = (
-    (df_comp['yhat'] == 0)
-    &
-    (df_comp['y_test'] == 1)
-)
-df_comp['1_pred_incorrect'] = (
-    (df_comp['yhat'] == 1)
-    &
-    (df_comp['y_test'] == 0)
-)
+        print('Best params for {}'.format(refit_score))
+        print(grid_search.best_params_)
 
+        # confusion matrix on the test data.
+        print('\nConfusion matrix of Random Forest optimized for {} on the test data:'.format(
+            refit_score
+        ))
+        print(pd.DataFrame(confusion_matrix(y_test, y_pred),
+                     columns=['pred_neg', 'pred_pos'], index=['neg', 'pos']))
+        return grid_search
+
+    # Call
+    grid_search_clf = grid_search_wrapper(refit_score='precision_score')
+    results = pd.DataFrame(grid_search_clf.cv_results_)
+    results = results.sort_values(by='mean_test_precision_score', ascending=False)
+    print(results.head())
