96e709e4141cf9602adfbfba787d6ad3a63223d1
PeterA182 <Altamura.Peter@gmail.com>
2019-09-20 20:46:07 -0400

    updated hr model and bash script

--- data/042_hr_prop_model.py
+++ data/042_hr_prop_model.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score, GridSearchCV, train_test_split, KFold, StratifiedKFold
 
 # Read in
 path = '/Volumes/Samsung_T5/mlb/gdApi/99_hr_prop_ft/featurespace_all.parquet'
@@ -89,4 +90,64 @@ print("Avg 0_pred_incorrect")
 print(np.mean(df_comp['0_pred_incorrect']))
 print("Avg 1_pred_incorrect")
 print(np.mean(df_comp['1_pred_incorrect']))
+print("---- ---- ---- ----\n"*4)
+
+# GridSearch
+param_grid = [
+        {'n_estimators': [7, 8, 10], 
+         'max_features': [0.1, 0.3, 0.4], 
+         'min_samples_split': [10], 
+         'min_samples_leaf': [10, 12, 15],
+         'n_jobs': [-1]}]
+rf_reg = RandomForestClassifier()
+grid_search = GridSearchCV(rf_reg, param_grid, cv=5, scoring="neg_mean_squared_error")
+grid_search.fit(X_train, y_train)
+print(param_grid)
+print(grid_search.best_params_)
+
+rf_clf_optimal = RandomForestClassifier(
+    n_estimators=grid_search.best_params_.get('n_estimators'), 
+    criterion='gini',
+    max_depth=None,
+    min_samples_split=grid_search.best_params_.get('min_samples_split'),
+    min_samples_leaf=grid_search.best_params_.get('min_samples_leaf'),
+    max_features=grid_search.best_params_.get('max_features')
+)
+rf_clf_optimal.fit(X_train, y_train)
+yhat = rf_clf_optimal.predict(X_test)
+pp = rf_clf_optimal.predict_proba(X_test)[:, 1]
+f1_score_rf = f1_score(y_test, yhat)
+acc_rf = accuracy_score(y_test, yhat)
+
+print("F1 Score")
+print(f1_score_rf)
+print("Acc Score")
+print(acc_rf)
+
+df_comp = pd.DataFrame({
+    'yhat': list(yhat),
+    'y_test': list(y_test),
+    'prob': list(pp)
+})
+df_comp.to_csv('/Users/peteraltamura/Desktop/rf_clf_hit_prop_optimal.csv', index=False)
+df_comp['0_pred_correct'] = (
+    (df_comp['yhat'] == 0)
+    &
+    (df_comp['y_test'] == 0)
+)
+df_comp['1_pred_correct'] = (
+    (df_comp['yhat'] == 1)
+    &
+    (df_comp['y_test'] == 1)
+)
+df_comp['0_pred_incorrect'] = (
+    (df_comp['yhat'] == 0)
+    &
+    (df_comp['y_test'] == 1)
+)
+df_comp['1_pred_incorrect'] = (
+    (df_comp['yhat'] == 1)
+    &
+    (df_comp['y_test'] == 0)
+)
 
--- data/daily_update.sh
+++ data/daily_update.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+cd /Users/peteraltamura/Documents/GitHub/liteSaber/data/
+python 000_bulk_pull.py
+python 010_standardize.py
+python 02_batter_stats_new.py
+python 02_pitcher_stats_new.py
+
